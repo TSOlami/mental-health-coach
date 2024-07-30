cd0401f46a976cc5cd48b83dcb6d58c7c1799c24
TSOlami <tijani.saheed@yahoo.com>
2024-07-30 11:42:30 +0100

    chore: Update SECRET_KEY handling in settings.py

--- mental_health_coach_backend/settings.py
+++ mental_health_coach_backend/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xem=vlnl3=t5rh1p508cd127fw@iqm^wu=^rolmab%tq)sea=)'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
