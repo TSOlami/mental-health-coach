b0302ae011355911664a9a67a69a68ca9b59f218
AbheeshKumar <abheshkhemani@gmail.com>
2024-07-30 19:03:06 +0500

    Branch

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,111 +1,66 @@
 "use client";
 
-import Image from "next/image";
+import { useState } from "react";
+import axios from "axios";
 import styles from "./page.module.css";
-import { useEffect, useState } from "react";
-import { Pathway_Service } from "./Service/pathway_service";
+import { FaMicrophone } from "react-icons/fa";
 
 export default function Home() {
-  const [inputValue, setInputValue] = useState({
-    skill: "",
-    num: "",
-    duration: "",
-  });
-  const [answer, setAnswer] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [image, setImage] = useState("");
-  const [parsedAnswer, setParsedAnswer] = useState();
-  console.log("JSON", parsedAnswer);
-  const parseLLM = (input) => {
-    const lines = input.trim().split("\n");
-    const result = {};
-    let currentMonth = "";
-    let currentWeek = "";
-    let currentProject = "";
-    lines.forEach((line) => {
-      line = line.trim();
-      line = line.replace(/[^\w\s]/g, "");
-      if (line.startsWith("Month")) {
-        currentMonth = line.replace(/\*\*/g, "").trim();
-        result[currentMonth] = { weeks: {}, project: "" };
-      } else if (line.startsWith("Week")) {
-        currentWeek = line.replace(/\*/g, "").trim();
-        result[currentMonth].weeks[currentWeek] = [];
-      } else if (line.startsWith("Project:")) {
-        currentProject = line.replace(/\*\*/g, "").trim();
-        result[currentMonth].project = currentProject;
-      } else if (line.startsWith("")) {
-        result[currentMonth]?.weeks[currentWeek]?.push(line.trim());
-      }
-    });
-    console.log(result);
-    return JSON.stringify(result, null, 2);
-  };
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
+  console.log(messages);
+  const handleMessage = async () => {
+    if (input.trim() == "") return;
 
-  const handleClick = async (e) => {
-    e.preventDefault();
-    setLoading(true);
+    const userMessage = { text: input, sender: "user" };
+    setMessages([...messages, userMessage]);
+    setInput("");
+
     try {
-      const res = await Pathway_Service(inputValue);
-      setAnswer(res.answer);
-      setImage(res.image);
+      const response = await axios.post("/api/chatbot", { message: input });
+      const botMessage = { text: response.data.reply, sender: "bot" };
+      setMessages([...messages, userMessage, botMessage]);
     } catch (error) {
-      console.log("Error Fetching Data", error);
-    } finally {
-      setLoading(false);
-      setParsedAnswer(parseLLM(answer));
+      console.error("Error Sending Message", error);
+      const errorMessage = {
+        text: "Sorry, I couldn't send process your request.",
+        sender: "bot",
+      };
+      setMessages([...messages, userMessage, errorMessage]);
     }
   };
-
-  const handleInput = (e) => {
-    e.preventDefault();
-    const { name, value } = e.target;
-    setInputValue((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
   return (
-    <main className={styles.main}>
-      <div>
-        <label>Write Skill</label>
-        <input
-          name="skill"
-          value={inputValue.skill}
-          onChange={handleInput}
-          type="text"
-        />
-        <label>Write total duration in numbers</label>
-        <input
-          name="num"
-          value={inputValue.num}
-          onChange={handleInput}
-          type="text"
-        />
-        <label>Write time format: day, month, year</label>
-        <input
-          name="duration"
-          value={inputValue.duration}
-          onChange={handleInput}
-          type="text"
-        />
-        <button onClick={handleClick}>Enter</button>
-      </div>
-
-      {loading ? (
-        <div>
-          <p>Loading....</p>
+    <main className={styles.container}>
+      <nav className={styles.logo}>
+        <h1>Anixety Bot</h1>
+      </nav>
+      <div className={styles.chatbox}>
+        <div className={styles.messages}>
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={`${styles.message} ${
+                msg.sender == "bot" ? styles.bot : styles.user
+              }`}
+            >
+              {msg.text}
+            </div>
+          ))}
         </div>
-      ) : (
-        <>
-          {/* {image && (
-            <Image width="350px" src={image} alt="AI Generated Image" />
-          )} */}
-          <pre>{answer}</pre>
-          <pre>{parsedAnswer}</pre>
-        </>
-      )}
+
+        <div className={styles.inputContainer}>
+          <textarea
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyPress={(e) => e.key === "Enter" && handleMessage()}
+            className={styles.input}
+          />
+          <div className={styles.btn}>
+            <FaMicrophone size={24} />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
