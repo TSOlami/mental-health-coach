23a7df3e6744b4b1afbeae0c2d1dc8f37338d6ff
TSOlami <tijani.saheed@yahoo.com>
2024-08-05 13:17:12 +0100

    Update allowed hosts in settings.py

--- backend/mental_health_coach_backend/settings.py
+++ backend/mental_health_coach_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mental-health-coach.onrender.com", "localhost"]
+ALLOWED_HOSTS = ["mental-health-coach.onrender.com", "localhost", "budgieai.vercel.app",]
 
 
 # Application definition
