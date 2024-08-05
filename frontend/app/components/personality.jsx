8e7c340ba5e4b9b04e551dc1f560f64722c00340
TSOlami <tijani.saheed@yahoo.com>
2024-08-05 15:30:47 +0100

    refactor: Update fetch URL in Chatbot component

--- frontend/app/components/personality.jsx
+++ frontend/app/components/personality.jsx
@@ -63,7 +63,7 @@ export default function Personalities({ setPersonality }) {
       console.log("Selected personality prompt:", systemPrompt);
 
       const response = await fetch(
-        "https://budgieai.vercel.app/api/chat/initialize/",
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/chat/initialize/`,
         {
           method: "POST",
           headers: {
