961021d467a30f45a310b3a7f0c6b68b80bdd978
TSOlami <tijani.saheed@yahoo.com>
2024-08-05 11:35:54 +0100

    feat: Add home path to urlpatterns
    Add the home path to the urlpatterns in urls.py, allowing users to access the home page of the application.

--- backend/mental_health_coach_backend/urls.py
+++ backend/mental_health_coach_backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from chat.views import home
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
-    path('api/chat/', include('chat.urls')),
+    path('api/', include('chat.urls')),
 ]
